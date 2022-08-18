78478ba1622c7958dd2df821edeb55b609dcbac8
IvaTsu <ivan.tsud@1337.tech>
2022-08-18 14:43:15 +0200

    refactor: put into 'images' state only needed data

--- src/components/SearchImages.js
+++ src/components/SearchImages.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import Results from "./Results";
 
 const SearchImages = () => {
-  const [images, setImages] = useState({});
+  const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -11,12 +11,14 @@ const SearchImages = () => {
   }, []);
 
   async function requestImages() {
+    // TODO: use .env to store this endpoint
     const res = await fetch(
       `https://www.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=4358fce2397bd1673106f92f24dd801b&user_id=tretton37&format=json&nojsoncallback=1`
     );
     const json = await res.json();
 
-    await setImages(json);
+    // put to the images what it expects to be, we can use a different state for the rest of the data
+    setImages(json?.photos?.photo);
     setLoading(false);
   }
 
