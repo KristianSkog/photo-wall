a66d9f65f799efa0811a5b73bd854e3a0b5c7b18
Kristian Skog <kristianskog@Kristians-MacBook-Pro.local>
2022-08-18 10:36:26 +0200

    Updated api call with new url

--- src/searchImages.js
+++ src/searchImages.js
@@ -1,39 +1,40 @@
 import { useState, useEffect } from "react";
 import Results from "./Results";
+import "./searchImages.css";
 
 const SearchImages = () => {
-  // const [images, setImages] = useState([]);
-
   const [images, setImages] = useState({});
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     requestImages();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  async function requestImages(searchText = "car") {
+  async function requestImages() {
     const res = await fetch(
-      `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=4358fce2397bd1673106f92f24dd801b&text=${searchText}&format=json&nojsoncallback=1`
+      `https://www.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=4358fce2397bd1673106f92f24dd801b&user_id=tretton37&format=json&nojsoncallback=1`
     );
     const json = await res.json();
 
     await setImages(json);
-
-    console.log("Ended");
-    // console.log("json", json);
+    setLoading(false);
   }
 
   return (
     <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          requestImages(e.target.querySelector(".searchInput").value);
-        }}
-      >
-        <input className="searchInput" />
-        <button>Submit</button>
-      </form>
-      <Results images={images} />
+      <div className="SearchForm__container">
+        <form
+          className="SearchForm"
+          onSubmit={(e) => {
+            e.preventDefault();
+            requestImages(e.target.querySelector(".searchInput").value);
+          }}
+        >
+          <input className="searchInput" placeholder="Search for images" />
+          <button className="searchSubmitButton">Submit</button>
+        </form>
+      </div>
+      {loading === false ? <Results images={images} /> : <p>Loading..</p>}
     </div>
   );
 };
